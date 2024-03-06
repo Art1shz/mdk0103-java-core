78d2c0cead04690d57edc8e2e753709d102abdd6
art1sh <art1shz@mail.ru>
2024-03-06 07:45:17 +0300

    exam

---
+++ src/game/King.java
@@ -0,0 +1,11 @@
+package game;
+public class King
+{
+    int hp, gold, score;
+    public King(int hp, int gold, int score)
+    {
+        this.hp = hp;
+        this.gold = gold;
+        this.score = score;
+    }
+}
